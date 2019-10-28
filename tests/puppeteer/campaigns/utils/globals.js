6b63a24e514eb80b50a45ce0f92ddff84fcc84b8
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-10-28 16:32:24 +0100

    Update SlowMo

--- tests/puppeteer/campaigns/utils/globals.js
+++ tests/puppeteer/campaigns/utils/globals.js
@@ -10,15 +10,15 @@ global.INSTALL = {
   URL: process.env.URL_INSTALL || `${global.FO.URL}install-dev/`,
   LANGUAGE: process.env.INSTALL_LANGUAGE || 'en',
   COUNTRY: process.env.INSTALL_COUNTRY || 'fr',
-  DB_USER: process.env.DB_USER || 'root',
-  DB_PASSWD: process.env.DB_PASSWD || '',
+  DB_USER: process.env.DB_USER || 'demops',
+  DB_PASSWD: process.env.DB_PASSWD || 'bribri',
   SHOPNAME: process.env.SHOPNAME || 'Prestashop',
   PS_VERSION: process.env.PS_VERSION || '1.7.6.0',
 };
 global.BROWSER_CONFIG = {
   headless: JSON.parse(process.env.HEADLESS || true),
   timeout: 0,
-  slowMo: 25,
+  slowMo: 5,
   args: ['--start-maximized', '--no-sandbox', '--lang=en-GB'],
   defaultViewport: {
     width: 1680,
