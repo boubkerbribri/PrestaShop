d20964febdda62b4d9ec279c5e42e02f8ccc9ce7
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-11-08 10:28:02 +0100

    add supplier to demo products

--- tests/puppeteer/campaigns/data/demo/products.js
+++ tests/puppeteer/campaigns/data/demo/products.js
@@ -1,6 +1,6 @@
 module.exports = {
   Products: {
-    demo_14: {id: '19', name: 'Customizable mug', reference: 'demo_14'},
-    demo_1: {id: '1', name: 'Hummingbird printed t-shirt', reference: 'demo_1'},
+    demo_14: {id: '19', name: 'Customizable mug', reference: 'demo_14', supplier: 'N/A'},
+    demo_1: {id: '1', name: 'Hummingbird printed t-shirt', reference: 'demo_1', supplier: 'N/A'},
   },
 };
