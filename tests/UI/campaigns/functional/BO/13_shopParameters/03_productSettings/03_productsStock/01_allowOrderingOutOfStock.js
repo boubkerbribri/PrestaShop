b7a611ffaa7df977deffd0373743a07e422fc7bb
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-06-04 12:06:59 +0200

    Add setup file that open browser only one time and close it at the end

--- tests/UI/campaigns/functional/BO/13_shopParameters/03_productSettings/03_productsStock/01_allowOrderingOutOfStock.js
+++ tests/UI/campaigns/functional/BO/13_shopParameters/03_productSettings/03_productsStock/01_allowOrderingOutOfStock.js
@@ -42,13 +42,12 @@ const init = async function () {
 describe('Allow ordering of out-of-stock products', async () => {
   // before and after functions
   before(async function () {
-    browser = await helper.createBrowser();
-    browserContext = await helper.createBrowserContext(browser);
+    browserContext = await helper.createBrowserContext(this.browser);
     page = await helper.newTab(browserContext);
     this.pageObjects = await init();
   });
   after(async () => {
-    await helper.closeBrowser(browser);
+    await helper.closeBrowserContext(browserContext);
   });
 
   // Login into BO and go to products page
