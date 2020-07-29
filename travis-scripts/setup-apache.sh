d426398c8b2ffa857b0f5c596d15291627178184
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-07-29 15:30:21 +0200

    Debug3 : install php extensions

--- travis-scripts/setup-apache.sh
+++ travis-scripts/setup-apache.sh
@@ -9,5 +9,14 @@ sudo cp -f tests/travis-ci-apache-vhost /etc/apache2/sites-available/000-default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
 sudo chmod 777 -R $HOME
 
+apt install -y \
+php7.4-zip \
+php7.4-xml \
+php7.4-curl \
+php7.4-intl \
+php7.4-mysql \
+php7.4-gd \
+php7.4-mbstring
+
 # Starting Apache
 sudo service apache2 restart
