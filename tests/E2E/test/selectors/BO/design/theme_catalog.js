75264110e072498d91378d52caedefeba4b94f71
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-11-13 14:04:58 +0100

    Fix some selectors

--- tests/E2E/test/selectors/BO/design/theme_catalog.js
+++ tests/E2E/test/selectors/BO/design/theme_catalog.js
@@ -1,13 +1,13 @@
 module.exports = {
   ThemeCatalog: {
-    discover_all_of_the_theme_button: '//*[@id="content" | @id="main-div"]//a[contains(text(), "Discover all of the themes")]',
+    discover_all_of_the_theme_button: '//a[contains(text(), "Discover all of the themes")]',
     category_name_text: '//*[@id="category_name"]',
-    discover_button: '(//*[@id="content" | @id="main-div"]//a[1]//p[contains(text(), "Discover")])[%POS]',
-    theme_name: '(//*[@id="content" | @id="main-div"]//a[1]//p[@class="bold"])[%POS]',
+    discover_button: '(//p[contains(text(), "Discover")])[%POS]',
+    theme_name: '(//a[contains(@class, "addons-theme-product-link")])[%POS]',
     search_addons_input: '//*[@id="addons-search-box"]',
 
     //Selectors in addons.prestashop.com site
     search_name: '//*[@id="search_name"]/b',
-    theme_header_name: '//*[@id="product_page"]//div[@class="product-title"]//h1'
+    theme_header_name: '//h1[@id="product_title"]'
   }
 };
